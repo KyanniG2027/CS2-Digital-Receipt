b5eecb18b873692fad1fd57f3a8dd44cd7aed2c6
Kyanni <kyanni.gonzalez2027@gbwl.org>
2025-10-24 12:55:47 +0000

    started new project

--- Main.java
+++ Main.java
@@ -1,6 +1,40 @@
-public class Main {
+   public class Main {
+  public static void main(String [] args) {
+     // RECEIPT PRINTOUT SECTION
 
-   public static void main(String []args) {
-      System.out.println("Hello World");
-   }
+     String highschoolName = "Birch Wathen Lenox";
+   
+      double drinkPrice = 1.50;
+      double candyPrice = 1.25;
+      double hotdogPrice = 2.75;
+      double hamburgerPrice = 3.50;
+   
+   
+   
+   
+   
+     System.out.println("**************************************");
+     System.out.println("*                                    *");
+     System.out.println("*   " + highschoolName + " Snack Bar *");
+     System.out.println("*                                    *");
+     System.out.println("*     Drink ..........$1.50 + drinkPrice         *");                      
+     System.out.println("*     Candy ..........$1.25          *");     
+     System.out.println("*     Hot Dog ........$2.75          *");     
+     System.out.println("*     Hamburger ......$3.50          *");     
+     System.out.println("*                                    *");    
+     System.out.println("**************************************");
+     }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 }
