6ad381a6d8f1c9faa4449b8fd72d0312af3e31ed
Kyanni <kyanni.gonzalez2027@gbwl.org>
2025-10-28 15:03:05 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-   public class Main {
+  import java.util.Scanner;
+  public class Main {
   public static void main(String [] args) {
      // RECEIPT PRINTOUT SECTION
+
+   Scanner input = new Scanner(System.in);
 
      String highschoolName = "Birch Wathen Lenox";
    
@@ -9,48 +12,52 @@
       double hotdogPrice = 2.75;
       double hamburgerPrice = 3.50;
    
-   
-   
-   
-   
-     System.out.println("**************************************");
-     System.out.println("*                                    *");
-     System.out.println("*   " + highschoolName + " Snack Bar *");
-     System.out.println("*                                    *");
-     System.out.println("*     Drink ..........$" + drinkPrice +"    *");                      
-     System.out.println("*     Candy + $" + candyPrice   +"     *");     
-     System.out.println("*     Hot Dog + $" + hotdogPrice +"      *");     
-     System.out.println("*     Hamburger + $" + hamburgerPrice +"   *");     
-     System.out.println("*                                    *");    
-     System.out.println("**************************************");
+
 
    int orderNumber = (int) (Math.random()* 100 + 1 );
-   System.out.println(orderNumber);
+   //System.out.println(orderNumber);
+   System.out.println("Order Number: " + orderNumber);
 
-   int drinksOrdered = (int) (Math.random()* 3 + 1 );
+   int drinksOrdered = input.nextInt();
+   //System.out.println(drinksOrdered);
+
+   int candiesOrdered = input.nextInt();
+   //System.out.println(candiesOrdered);
+
+   int hotDogs = input.nextInt();
+   //System.out.println(hotDogs);
+
+   int hamburgersOrdered = input.nextInt();
+   //System.out.println(hamburgersOrdered);
+
+   
    System.out.println(drinksOrdered);
-
-   int candiesOrdered = (int) (Math.random() * 3 + 1 );
    System.out.println(candiesOrdered);
-
-   int hotDogs = (int) (Math.random()* 3 + 1 );
    System.out.println(hotDogs);
-
-   int hamburgersOrdered = (int) (Math.random() * 3 + 1 );
    System.out.println(hamburgersOrdered);
 
 
 
+
    double taxRate = 0.08;
-   double subtotal = priceDrink *  drinksOrdered + candiesOrdered + hotDogs + hamburgersOrdered; 
+   double subtotal = drinkPrice *  drinksOrdered + candyPrice * candiesOrdered + hotdogPrice * hotDogs + hamburgerPrice * hamburgersOrdered; 
    double totalTax = subtotal * taxRate;
    double total = subtotal + totalTax; 
+   //System.out.println(subtotal);
+   //System.out.println(totalTax);
+   //System.out.println(total);
 
 
-
-
-
-
+    System.out.println("**************************************");
+     System.out.println("*    "    + orderNumber  + "                       *");
+     System.out.println("*   " + highschoolName + " Snack Bar *");
+     System.out.println("*                                    *");
+     System.out.println("*     Drink ..........$" + drinkPrice +" *");                      
+     System.out.println("*     Candy + $" + candyPrice   +"     *");     
+     System.out.println("*     Hot Dog + $" + hotdogPrice +"    *");     
+     System.out.println("*     Hamburger + $" + hamburgerPrice +"*");     
+     System.out.println("*     Total + $" + total + "                     *");    
+     System.out.println("**************************************");
 
 
      }
